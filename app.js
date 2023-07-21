6617e5d8de12d16999361acd42c525e9c990b976
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-21 10:18:02 +0200

    control

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const canvasWidth = window.innerWidth;
-const canvasHeight = window.innerHeight;
+let canvasWidth = window.innerWidth;
+let canvasHeight = window.innerHeight;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
@@ -10,10 +10,17 @@ let animate;
 const player = new Player((canvasWidth/2), (canvasHeight/2), 50, 50);
 
 function animation() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
     animate = requestAnimationFrame(animation);
+
+    // canvasWidth = window.innerWidth;
+    // canvasHeight = window.innerHeight;
+    // canvas.width = canvasWidth;
+    // canvas.height = canvasHeight;
 
     if (player) {
         player.draw(ctx);
+        player.control();
     }
 }
 animation();
--- player.js
+++ player.js
@@ -10,5 +10,51 @@ class Player extends GameObject {
         super.draw(ctx);
     }
 
-    
+    control() {
+        document.onkeydown = (keyevent => {
+            this.controller[keyevent.key] = true;
+        });
+
+        document.onkeyup = (keyevent => {
+            this.controller[keyevent.key] = false;
+        });
+
+        console.log(this.controller);
+        
+        for (const key in this.controller) {
+            if (key.includes("Left") && this.controller[key]) {
+                this.x = this.x - this.speed;
+            }
+            if (key.includes("Right") && this.controller[key]) {
+                this.x = this.x + this.speed;
+            }
+            if (key.includes("Up") && this.controller[key]) {
+                this.y = this.y - this.speed;
+            }
+            if (key.includes("Down") && this.controller[key]) {
+                this.y = this.y + this.speed;
+            }
+        }
+
+        // switch (keyevent.key) {
+        //     case "ArrowLeft":
+        //         this.x = this.x - this.speed;
+        //         break;
+
+        //     case "ArrowRight":
+        //         this.x = this.x + this.speed;
+        //         break;
+
+        //     case "ArrowUp":
+        //         this.y = this.y - this.speed;
+        //         break;
+
+        //     case "ArrowDown":
+        //         this.y = this.y + this.speed;
+        //         break;
+            
+        //     default:
+        //         break;
+        // }
+    }
 }
